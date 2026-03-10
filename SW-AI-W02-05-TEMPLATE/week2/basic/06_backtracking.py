47dcfb91ab79ec71bff9986286172cc572b6857c
Gyutae Kim <robertkim5363@gmail.com>
2026-03-10 09:56:05 +0900

    Add backtracking logic to generate combinations
    Implemented base case and backtracking logic for combinations.

--- SW-AI-W02-05-TEMPLATE/week2/basic/06_backtracking.py
+++ SW-AI-W02-05-TEMPLATE/week2/basic/06_backtracking.py
@@ -44,14 +44,18 @@ def combinations(n, k):
             current_combination: 현재까지 선택한 숫자들
         """
         # TODO: base case - k개를 모두 선택했으면 결과에 추가
-        pass
-        
+        if len(current_combination) == k:
+            result.append(current_combination[:])
+            return
+        for i in range(start, n+1):
+            current_combination.append(i)
+            backtrack(i+1,current_combination)
+            current_combination.pop()
         # TODO: start부터 n까지 숫자를 하나씩 시도
         ## TODO: 백트랙킹 3단계 구현
         ## 1. 선택(Choose)
         ## 2. 탐색(Explore)
         ## 3. 취소(Unchoose)
-        pass
     
     backtrack(1, [])
     return result
@@ -95,4 +99,3 @@ if __name__ == "__main__":
     result4 = combinations(n4, k4)
     print(f"C({n4}, {k4}) = {result4}")
     print(f"총 {len(result4)}개의 조합")
-
