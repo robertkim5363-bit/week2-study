0894fd8f6795905e61153e6cb35d8469bab55b9c
Gyutae Kim <robertkim5363@gmail.com>
2026-03-11 09:05:38 +0900

    Implement maximum value finder in Python
    Added code to find the maximum value and its position from user input.

--- SW-AI-W02-05-TEMPLATE/week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ SW-AI-W02-05-TEMPLATE/week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,15 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+
+nums = []
+
+# Loop 9 times (range(1, 10) gives you 1, 2, 3, 4, 5, 6, 7, 8, 9)
+for i in range(1, 10):
+    num = int(input())
+    nums.append(num)  # The list appends the number!
+
+max_val = max(nums)   # Use a safe variable name
+max_position = nums.index(max_val) + 1
+
+print(max_val)
+print(max_position)
